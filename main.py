b65f0766a30dbc8560b0b0ab934d4ca5b8540cf1
sysopmatt <mattpadams@gmail.com>
2021-09-18 10:21:19 -0700

    Created initial API

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pyodbc
 import confuse
+from flask import Flask, request, jsonify
+import os
 
 config = confuse.Configuration('fluffy-api', __name__)
 
@@ -12,11 +14,27 @@ connection = pyodbc.connect(
     port=1433,
     driver='/usr/local/lib/libtdsodbc.so'
 )
+ApiKeys = list()
 
 cursor = connection.cursor()
-cursor.execute("SELECT * FROM TW")
-row = cursor.fetchone()
-while row:
-    print (row)
-    row = cursor.fetchone()
+cursor.execute("SELECT DISTINCT ApiKey FROM FluffyUsers")
+row = cursor.fetchall()
+for key in row:
+    ApiKeys.append(key[0])
+cursor.close()
 
+app = Flask(__name__)
+
+
+@app.route('/Upload/', methods=['POST'])
+def index():
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+    if auth in ApiKeys:
+        return jsonify({"message": "OK: Authorized"}), 200
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
+
+
+if __name__ == '__main__':
+    app.run(port = int(os.environ.get('PORT', 5000)))
