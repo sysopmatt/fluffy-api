95ca7d45dab26d282e911692d7532eae1eafe956
sysopmatt <mattpadams@gmail.com>
2021-09-17 20:58:20 -0700

    Basic SQL connection using confuse for yaml configuration

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import pyodbc
+import confuse
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+config = confuse.Configuration('fluffy-api', __name__)
 
+connection = pyodbc.connect(
+    server=config['sql']['server'].get(),
+    database=config['sql']['database'].get(),
+    user=config['sql']['user'].get(),
+    tds_version='7.4',
+    password=config['sql']['password'].get(),
+    port=1433,
+    driver='/usr/local/lib/libtdsodbc.so'
+)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM TW")
+row = cursor.fetchone()
+while row:
+    print (row)
+    row = cursor.fetchone()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
